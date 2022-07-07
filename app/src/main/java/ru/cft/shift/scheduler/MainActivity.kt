6de8b8d6e2d907fbf4d04821d70e6de14085a104
MaksimAkimov2003 <a.t.n20034@gmail.com>
2022-07-07 22:44:01 +0700

    Add registration and input

--- app/src/main/java/ru/cft/shift/scheduler/MainActivity.kt
+++ app/src/main/java/ru/cft/shift/scheduler/MainActivity.kt
@@ -1,14 +1,21 @@
 package ru.cft.shift.scheduler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.Gravity
+import android.view.MotionEvent
 import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
+import com.google.android.material.textfield.TextInputLayout
 import ru.cft.shift.scheduler.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
+//private lateinit var detector: GestureDetectorCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +26,52 @@ class MainActivity : AppCompatActivity() {
             dataChecker()
         }
 
+        binding.buttonInviteToRegistration.setOnClickListener {
+            var intent = Intent(MainActivity@this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        //detector = GestureDetectorCompat(this, DiaryGestureListener())
+
     }
+
+//    override fun onTouchEvent(event: MotionEvent?): Boolean {
+//        return if (detector.onTouchEvent(event)) {
+//            true
+//        }
+//
+//        else {
+//            return super.onTouchEvent(event)
+//        }
+//    }
+
+//    inner class DiaryGestureListener: GestureDetector.SimpleOnGestureListener() {
+//        private val SWIPE_TRESHOLD = 100
+//        private val SWIPE_VELOCITY_TRESHOLD = 100
+//
+//        override fun onFling(
+//            downEvent: MotionEvent?,
+//            moveEvent: MotionEvent?,
+//            velocityX: Float,
+//            velocityY: Float
+//        ): Boolean {
+//            var diffx = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+//            var diffy = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+//
+//             return if (Math.abs(diffx) > Math.abs(diffy)) {
+//                 if (Math.abs(diffx) > SWIPE_TRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_TRESHOLD) {
+//                     if (diffx > 0) {
+//                         binding.textInputLayoutPassword.OnSwipeRight()
+//                     }
+//                 }
+//             }
+//
+//
+//        }
+//    }
+
 
     private fun dataChecker() {
         """^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*${'$'}""".toRegex() //регулярка для пароля
@@ -50,5 +102,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    
+
+
+
+}
+
+private fun TextInputLayout.onSwipeRight() {
+    binding.password.setText("")
 }
